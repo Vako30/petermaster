1b43515a3a6df545e21aeada515b4f2b3095a235
VahanH <vahanius@gmail.com>
2015-02-12 10:21:28 +0400

    home template html is done

--- template-custom.php
+++ template-custom.php
@@ -4,89 +4,165 @@ Template Name: Home
 */
 ?>
 <div id="owl-example" class="owl-carousel">
-        <div class="general-slider">
-            <div class="sl-items">
-                <img src="..." alt="">
-                <img src="..." alt="">
-                <img src="..." alt="">
-            </div>
 
-            <div class="owl-under-img-text">
-                <h1></h1>
-                <p></p>
-            </div>
+    <div class="general-slider">
+
+        <div class="sl-items">
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide1.jpg" alt="">
+
         </div>
+
+
+        <div class="owl-under-img-text">
+
+            <h1></h1>
+
+            <p></p>
+
+        </div>
+
+    </div>
+
 </div>
 
+<div class="row well">
 
-        <div class="row well">
-            <h1 class="text-center">Загаловок H1</h1>
+    <h1 class="text-center">Загаловок H1</h1>
 
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
+
+    <div class="col-sm-6 col-md-4">
+
+        <div class="thumbnail">
+
+            <div class="caption">
+
+                <h3>Thumbnail label</h3>
+
             </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/538710492.jpg"" alt="...">
+
         </div>
-        <div class="row well">
-            <h2 class="text-center">Загаловок H2</h2>
 
-            <p class="">
-                Stylized implementation of HTML's element for abbreviations and acronyms to show the
-                expanded version on hover. Abbreviations with a title attribute have a light dotted bottom border
-                and a help cursor on hover, providing additional context on hover and to users of assistive
-                technologies.
-            </p>
-        </div>
-        <div class="row well">
-            <h3 class="text-center">Загаловок H3</h3>
+    </div>
 
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="..." alt="...">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row well">
-            <h4 class="text-center">Загаловок H4</h4>
+    <div class="col-sm-6 col-md-4">
 
-            <p class="text-center">Загаловок H3 dfshxfgjnyjh dhd fhgfdxj fghd d gh ghdf</p>
+        <div class="thumbnail">
+
+            <div class="caption">
+
+                <h3>Thumbnail label</h3>
+
+            </div>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide1.jpg"" alt="...">
+
         </div>
+
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+
+        <div class="thumbnail">
+
+            <div class="caption">
+
+                <h3>Thumbnail label</h3>
+
+            </div>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/538710492.jpg"" alt="...">
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="row well">
+
+    <h2 class="text-center">Загаловок H2</h2>
+
+
+    <p class="">
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+
+        Dolore esse illo ipsa ipsam itaque iusto sed voluptate?
+
+        Ad aliquam assumenda consequatur cum dolor dolorem doloremque
+
+        dolores dolorum et, excepturi facere facilis fugiatimpedit, in incidunt labore
+
+        magnam modi molestiae optio, quas quibusdam reprehenderit rerum vitae. Dolore id minus reiciendis veniam!
+
+    </p>
+
+</div>
+
+<div class="row well">
+
+    <h3 class="text-center">Загаловок H3</h3>
+
+
+    <div class="col-sm-6 col-md-4">
+
+        <div class="thumbnail">
+
+            <div class="caption">
+
+                <h3>Thumbnail label</h3>
+
+            </div>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/538710492.jpg"" alt="...">
+
+        </div>
+
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+
+        <div class="thumbnail">
+
+            <div class="caption">
+
+                <h3>Thumbnail label</h3>
+
+            </div>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/2lenovoideapady50059345640.jpg"" alt="...">
+
+        </div>
+
+    </div>
+
+    <div class="col-sm-6 col-md-4">
+
+        <div class="thumbnail">
+
+            <div class="caption">
+
+                <h3>Thumbnail label</h3>
+
+            </div>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/538710492.jpg"" alt="...">
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="row well">
+
+    <h4 class="text-center">Загаловок H4</h4>
+
+
+    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda aut blanditiis
+        iste labore magnam non rerum sit tempore vel. </p>
+
+</div>
