05c6b64bada4674741dd5b07ccc6e0491aff25c2
VahanH <vahanius@gmail.com>
2015-02-13 17:54:58 +0400

    reviews updated

--- template-reviews.php
+++ template-reviews.php
@@ -10,7 +10,8 @@
     <div class="alert alert-info media">
         <div class="media-left">
             <a href="#">
-                <img class="img-circle media-object" src="<?php echo get_template_directory_uri(); ?>/assets/img/538710492.jpg" alt="...">
+                <img class="img-circle media-object"
+                     src="<?php echo get_template_directory_uri(); ?>/assets/img/538710492.jpg" alt="...">
             </a>
         </div>
         <div class="media-body">
@@ -31,31 +32,50 @@
         </ul>
     </nav>
     <div class="row">
-        <form role="form" action="" method="post" >
+        <form role="form" action="" method="post">
             <div class="col-lg-6">
                 <h1 class="text-center">Оставить отзыв</h1>
+
                 <div class="form-group">
                     <label for="InputName">Заголовок</label>
+
                     <div class="input-group">
-                        <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Заголовок" required>
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+                        <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Заголовок"
+                               required>
+                        <span class="input-group-addon"><i
+                                class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
                 </div>
                 <div class="form-group">
                     <label for="InputName">Имя</label>
+
                     <div class="input-group">
-                        <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Имя" required>
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+                        <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Имя"
+                               required>
+                        <span class="input-group-addon"><i
+                                class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+                </div>
+                <div class="form-group">
+                    <label for="InputName">Город</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Город"
+                               required>
+                        <span class="input-group-addon"><i
+                                class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
                 </div>
                 <div class="form-group">
                     <label for="InputEmail">Email</label>
+
                     <div class="input-group">
-                        <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="example@gmail.com" required  >
+                        <input type="email" class="form-control" id="InputEmail" name="InputEmail"
+                               placeholder="example@gmail.com" required>
                         <span class="input-group-addon"><i class="icon-envelope"></i></span></div>
                 </div>
                 <div class="form-group">
                     <label for="InputMessage">Сообщение</label>
+
                     <div class="input-group">
-                        <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
+                        <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5"
+                                  required></textarea>
                         <span class="input-group-addon"><i class="icon-pencil"></i></span></div>
                 </div>
                 <input type="submit" name="submit" id="submit" value="ОТПРАВИТЬ" class="btn btn-info pull-right">
