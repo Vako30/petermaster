3a53b418624e8075b39e0d502c157070541421c0
VahanH <vahanius@gmail.com>
2015-02-13 18:56:46 +0400

    price-list updated

--- template-price-list.php
+++ template-price-list.php
@@ -7,13 +7,40 @@
 <div class="no-margin row well">
 
     <h1 class="text-center">Прайс-Лист</h1>
+    <div role="tabpanel">
 
-    <ul class="list-group">
-        <li class="list-group-item"><a href="">Услуги по электрике</a></li>
-        <li class="list-group-item"><a href="">Услуги по сантехнике</a></li>
-        <li class="list-group-item"><a href="">Столярно-плотницкие работы</a></li>
-        <li class="list-group-item"><a href="">Мелкий бытовой ремонт</a></li>
-        <li class="list-group-item"><a href="">Установка бытовой техники</a></li>
-        <li class="list-group-item"><a href="">Устранение засоров</a></li>
-    </ul>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab">Услуги по электрике</a></li>
+            <li role="presentation"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab">Услуги по сантехнике</a></li>
+            <li role="presentation"><a href="#tab3" aria-controls="tab3" role="tab" data-toggle="tab">Столярно-плотницкие работы</a></li>
+            <li role="presentation"><a href="#tab4" aria-controls="tab4" role="tab" data-toggle="tab">Мелкий бытовой ремонт</a></li>
+            <li role="presentation"><a href="#tab4" aria-controls="tab5" role="tab" data-toggle="tab">Установка бытовой техники</a></li>
+            <li role="presentation"><a href="#tab4" aria-controls="tab6" role="tab" data-toggle="tab">Устранение засоров</a></li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="tab1">
+                <p>
+                    Наименование работ	Стоимость работ</br>
+                    Стоимость прокладки провода</br>
+                    Прокладка открытой электропроводки по бетону или кирпичу	200 руб. за п.м.</br>
+                    Прокладка открытой электропроводки по мягкой поверхности, плинтусу или под подвесным потолком	150 руб. за п.м.</br>
+                    Прокладка открытой электропроводки по потолку	300 руб. за п.м.</br>
+                    Прокладка скрытой электропроводки по бетону или кирпичу (штробление)	от 350 руб. за п.м.</br>
+                    Прокладка скрытой электропроводки по потолку (штробление)	от 500 руб. за п.м.</br>
+                    Прокладка скрытой электропроводки по мягкой поверхности	от 250 руб. за п.м.</br>
+                    Оштукатуривание штробы	от 50 руб. за п.м.</br>
+                    Изоляция контактов	100 руб.</br>
+                </p>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="tab2">afsdfdsfdsafadfd</div>
+            <div role="tabpanel" class="tab-pane" id="tab3">adf bdf aedf s\ a.</div>
+            <div role="tabpanel" class="tab-pane" id="tab4">.adf df reqfad fead df ce fd</div>
+            <div role="tabpanel" class="tab-pane" id="tab5">.adf df reqfad fead df ce fd</div>
+            <div role="tabpanel" class="tab-pane" id="tab6">.adf df reqfad fead df ce fd</div>
+        </div>
+
+    </div>
 </div>
