25372444222a85342046a82408040b3afe1c6f54
VahanH <vahanius@gmail.com>
2015-02-13 15:42:06 +0400

    header updated

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 navbar-header">
+            <div class=" col-md-4 navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
@@ -10,7 +10,7 @@
                 </button>
                 <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo4.png" class="img-responsive" alt=""></a>
             </div>
-            <div class="col-md-8">
+            <div class="col-sm-12 col-md-8">
                 <nav class=" collapse navbar-collapse" role="navigation">
 
                     <div class="col-xs-12 ">
