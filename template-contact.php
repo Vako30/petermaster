291fb9a98a66ebe1a31e6c4f850cd5d3babe778e
VahanH <vahanius@gmail.com>
2015-02-13 20:00:48 +0400

    price-list updated

--- template-contact.php
+++ template-contact.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<div class="">
+<div class="map">
     <script type="text/javascript" charset="utf-8"
-            src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=kcol9EMdJR-8bbNDzHp5SE8pqASFp_vA&width=100%&height=450"></script>
+            src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=kcol9EMdJR-8bbNDzHp5SE8pqASFp_vA&width=100%&height=700"></script>
 </div>
