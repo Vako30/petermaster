f813d7642d229064d24f7d02ef758485d82e2e65
Smart <petrosyan.vahe@rambler.ru>
2015-02-16 21:30:31 +0400

    comments

--- templates/footer.php
+++ templates/footer.php
@@ -2,31 +2,32 @@
     <div class="container">
         <div class="row no-margin footer_cont well">
             <?php dynamic_sidebar('sidebar-footer'); ?>
+            <?php $query = new WP_Query(array('category_name' => 'footer'));
+            if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <div class="caption">
+                            <? the_content() ?>
+                        </div>
+                    </div>
+                </div>
+            <?
+            endwhile;
+            endif;
+            // Reset Query
+            //wp_reset_query();
+            ?>
 
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xs-12 text-center alert alert-info"> Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit. Autem ducimus, libero neque quos reiciendis vel!
+            <div class="col-xs-12 text-center alert alert-info">
+                <?php $query = new WP_Query(array('category_name' => 'footerdown'));
+                if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+                             <? the_content() ?>
+                <?
+                endwhile;
+                endif;
+                // Reset Query
+                //wp_reset_query();
+                ?>
             </div>
         </div>
     </div>
