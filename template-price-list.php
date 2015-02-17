e6ba6e681a9afcd3fad1773118dcb983867414f8
VahanH <vahanius@gmail.com>
2015-02-17 13:41:09 +0400

    template-price-list added wordpress

--- template-price-list.php
+++ template-price-list.php
@@ -9,204 +9,32 @@
     <h1 class="text-center">Прайс-Лист</h1>
 
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingOne">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true"
-                       aria-controls="collapseOne">
-                        Услуги по электрике
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                <div class="panel-body">
-                    Наименование работ	Стоимость работ</br>
-                    Стоимость прокладки провода</br>
-                    Прокладка открытой электропроводки по бетону или кирпичу	200 руб. за п.м.</br>
-                    Прокладка открытой электропроводки по мягкой поверхности, плинтусу или под подвесным потолком	150 руб. за п.м.</br>
-                    Прокладка открытой электропроводки по потолку	300 руб. за п.м.</br>
-                    Прокладка скрытой электропроводки по бетону или кирпичу (штробление)	от 350 руб. за п.м.</br>
-                    Прокладка скрытой электропроводки по потолку (штробление)	от 500 руб. за п.м.</br>
-                    Прокладка скрытой электропроводки по мягкой поверхности	от 250 руб. за п.м.</br>
-                    Оштукатуривание штробы	от 50 руб. за п.м.</br>
-                    Изоляция контактов	100 руб.</br>
-                    Демонтаж соединительной коробки	400 руб.</br>
-                    Установка соединительной коробки	550 руб.</br>
-                    Ремонт соединительной коробки (открытая проводка)	500 руб.</br>
-                    Демонтаж плинтуса	100 руб. за п.м.</br>
-                    Установка плинтуса	100 руб. за п.м.</br>
-                    Установка кабель-канала	от 100 до 200 руб. за п.м.</br>
-                    Стоимость установки автоматов, электрощитов, УЗО и сопутствующих электромонтажных работ</br>
-                    Демонтаж трехфазного автомата	300 руб.</br>
-                    Установка трехфазного автомата	600 руб.</br>
-                    Демонтаж однофазного автомата	150 руб.</br>
-                    Установка однофазного автомата	250 руб.</br>
-                    Демонтаж УЗО	200 руб.</br>
-                    Установка УЗО	350 руб.</br>
-                    Демонтаж пакетника	150 руб.</br>
-                    Установка пакетника	300 руб.</br>
-                    Демонтаж щита	от 500 до 1000 руб.</br>
-                    Установка щита	700 руб.</br>
-                    Ремонт щита (без замены автоматов)	750 руб.</br>
-                    Установка DIN-рейки	150 руб.</br>
-                    Установка заземляющей шинки	200 руб.</br>
-                    Штробление под щит (500х300 мм)	2500 руб.</br>
-                    Стоимость установки и ремонта выключателей и розеток</br>
-                    Демонтаж выключателя	200 руб.</br>
-                    Установка выключателя	350 руб.</br>
-                    Ремонт выключателя	350 руб.</br>
-                    Демонтаж блока выключателей	300 руб.</br>
-                    Установка блока выключателей	550 руб.</br>
-                    Ремонт блока выключателей	500 руб.</br>
-                    Демонтаж розетки	250 руб.</br>
-                    Установка розетки	400 руб.</br>
-                    Ремонт розетки	350 руб.</br>
-                    Монтаж электроточки в ГКЛ	250 руб.</br>
-                    Монтаж электроточки в кирпичной стене	450 руб.</br>
-                    Монтаж электроточки в бетонной стене	550 руб.</br>
-                    Демонтаж звонка	150 руб.</br>
-                    Установка звонка	250 руб.</br>
-                    Установка кнопки звонка	250 руб.</br>
-                    Установка блоков управления	от 600 до 1200 руб.</br>
-                    Стоимость подключения люстр и светильников</br>
-                    Демонтаж люстры	300 руб.</br>
-                    Сборка люстры	от 450 руб.</br>
-                    Подвес люстры	от 450 руб.</br>
-                    Ремонт люстры (без демонтажа)	500 руб.</br>
-                    Установка крюка для люстры	200 руб.</br>
-                    Установка планки для люстры	350 руб.</br>
-                    Демонтаж патрона	150 руб.</br>
-                    Установка патрона	200 руб.</br>
-                    Демонтаж светильника	200 руб.</br>
-                    Установка светильника	от 300 руб.</br>
-                    Монтаж трансформатора 12в	от 350 руб.</br>
-                    Ремонт светильника или бра	от 400 руб.</br>
-                    Ремонт лампы дневного света	450 руб.</br>
-                    Ремонт галогенового светильника	500 руб.</br>
-                    Стоимость сопутствующих электромонтажных работ</br>
-                    Бурение отверстия под дюбель диаметром 6-10 мм	50 руб.</br>
-                    Бурение отверстия под дюбель диаметром 11-16 мм	100 руб.</br>
-                    Бурение отверстия под дюбель диаметром более 16 мм	от 200 руб.</br>
-                    Бурение отверстия в бетоне или кирпиче	от 350 руб.</br>
-                    Бурение отверстия в мягкой поверхности	300 руб.</br>
-                    Ремонт электропроводки	550 руб.</br>
-                    Прозвонка электропроводки светоиндикатором	150 руб.</br>
-                    Прозвонка скрытой электропроводки спецприбором	100 руб. за п.м.</br>
-                    Определение элементов скрытой конструкции	100 руб. за ед.</br>
-                    Оштукатуривание стены	300 руб. за м2</br>
-                    Замена лампочки	от 50 до 150 руб.</br>
-                    Мелкий ремонт электрики	550 руб.</br>
-                    Покупка товара	350 руб. за час</br>
-                    Поиск неисправности электрики	1000 руб. за час</br>
-                    Составление сметы	500 руб.</br>
-                    Коэффициент стесненности	30%</br>
+        <?php $query = new WP_Query(array('category_name' => 'price'));
+        if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#<?= get_the_ID() ?>"
+                           aria-expanded="true"
+                           aria-controls="<?= get_the_ID() ?>">
+                            <? the_title() ?>
+                        </a>
+                    </h4>
+                </div>
+                <div id="<?= get_the_ID() ?>" class="panel-collapse collapse" role="tabpanel"
+                     aria-labelledby="heading">
+                    <div class="panel-body">
+                        <? the_content() ?>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingTwo">
-                <h4 class="panel-title">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"
-                       aria-expanded="false" aria-controls="collapseTwo">
-                        Услуги по сантехнике
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                    sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                    labore sustainable VHS.
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingThree">
-                <h4 class="panel-title">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree"
-                       aria-expanded="false" aria-controls="collapseThree">
-                        Столярно-плотницкие работы
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-                <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                    sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                    labore sustainable VHS.
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingFour">
-                <h4 class="panel-title">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour"
-                       aria-expanded="false" aria-controls="collapseThree">
-                        Мелкий бытовой ремонт
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
-                <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                    sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                    labore sustainable VHS.
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingFive">
-                <h4 class="panel-title">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive"
-                       aria-expanded="false" aria-controls="collapseFive">
-                        Установка бытовой техники
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
-                <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                    sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                    labore sustainable VHS.
-                </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingSix">
-                <h4 class="panel-title">
-                    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix"
-                       aria-expanded="false" aria-controls="collapseSix">
-                        Устранение засоров
-                    </a>
-                </h4>
-            </div>
-            <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
-                <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                    sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                    labore sustainable VHS.
-                </div>
-            </div>
-        </div>
+        <?
+        endwhile;
+        endif;
+        // Reset Query
+        //wp_reset_query();
+        ?>
     </div>
+
+
 </div>
