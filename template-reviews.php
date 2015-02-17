d9b9dc7396033263b421d8491be821652b685dcb
Smart <petrosyan.vahe@rambler.ru>
2015-02-17 20:13:13 +0400

    comments

--- template-reviews.php
+++ template-reviews.php
@@ -4,6 +4,11 @@
  */
 ?>
 <div class="no-margin row well">
-    <?php comments_template(); ?>
+    <?php foreach (get_comments() as $comment): ?>
+        <div><?php echo $comment->comment_author; ?> said: "<?php echo $comment->comment_content; ?>".</div>
+    <?php endforeach; ?>
 </div>
+
+
+<?php comment_form(); ?>
 
