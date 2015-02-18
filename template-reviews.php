eb9b3d24f327743e23893377e14baa8e5e56c719
VahanH <vahanius@gmail.com>
2015-02-18 18:09:34 +0400

    template-review updated

--- template-reviews.php
+++ template-reviews.php
@@ -8,14 +8,25 @@
         <div class="row">
             <div>
                 <strong class="col-xs-12 col-md-8"><?php echo $comment->comment_author; ?></strong></br>
-                <div class="col-xs-12 col-md-9"><?php echo $comment->comment_date; ?>.</div></br>
+                <time class="col-xs-12 col-md-9"><?php echo $comment->comment_date; ?></time>
+                </br>
+                <div class="col-xs-12 col-md-9"><?php echo $comment->comment_type; ?></div>
+                </br>
                 <div class="col-xs-12 col-md-9"><?php echo $comment->comment_content; ?>.</div>
-                <div class="col-xs-12 col-md-9"><hr></div>
+                <div class="col-xs-12 col-md-9">
+                    <hr>
+                </div>
             </div>
         </div>
     <?php endforeach; ?>
 </div>
 <?php $comment_args = array('fields' => apply_filters('comment_form_default_fields', array(
+    'comment_type' => '<p class="comment-form-type">' .
+        '<label for="type">' . __('Заголовок *') . '</label> ' .
+        ($req ? '<span class="required">*</span>' : '') .
+        '<input id="comment_type" name="comment_type" type="text" value="' .
+        esc_attr($commenter['comment_type']) . '" size="30"' . $aria_req . ' />' .
+        '</p><!-- #form-section-author .form-section -->',
     'author' => '<p class="comment-form-author">' .
         '<label for="author">' . __('Имя *') . '</label> ' .
         ($req ? '<span class="required">*</span>' : '') .
@@ -30,7 +41,7 @@
     'url' => '')),
     'comment_field' => '<p class="comment-form-comment">' .
         '<label for="comment">' . __('Сообщение:') . '</label>' .
-        '<textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>' .
+        '<textarea id="comment" name="comment" cols="45" rows="10" aria-required="true"></textarea>' .
         '</p><!-- #form-section-comment .form-section -->',
     'comment_notes_after' => '',
 );
