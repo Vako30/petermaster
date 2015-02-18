11650b30bad9215f55b353eeff4ee50c89723cea
VahanH <vahanius@gmail.com>
2015-02-18 17:38:39 +0400

    template-review updated

--- template-reviews.php
+++ template-reviews.php
@@ -5,8 +5,36 @@
 ?>
 <div class="no-margin row well">
     <?php foreach (get_comments() as $comment): ?>
-        <div><?php echo $comment->comment_author; ?> said: "<?php echo $comment->comment_content; ?>".</div>
+        <div class="row">
+            <div>
+                <strong class="col-xs-12 col-md-8"><?php echo $comment->comment_author; ?></strong></br>
+                <div class="col-xs-12 col-md-9"><?php echo $comment->comment_date; ?>.</div></br>
+                <div class="col-xs-12 col-md-9"><?php echo $comment->comment_content; ?>.</div>
+                <div class="col-xs-12 col-md-9"><hr></div>
+            </div>
+        </div>
     <?php endforeach; ?>
 </div>
-<?php comment_form(); ?>
+<?php $comment_args = array('fields' => apply_filters('comment_form_default_fields', array(
+    'author' => '<p class="comment-form-author">' .
+        '<label for="author">' . __('Имя *') . '</label> ' .
+        ($req ? '<span class="required">*</span>' : '') .
+        '<input id="author" name="author" type="text" value="' .
+        esc_attr($commenter['comment_author']) . '" size="30"' . $aria_req . ' />' .
+        '</p><!-- #form-section-author .form-section -->',
+    'email' => '<p class="comment-form-email">' .
+        '<label for="email">' . __('Email *') . '</label> ' .
+        ($req ? '<span class="required">*</span>' : '') .
+        '<input id="email" name="email" type="text" value="' . esc_attr($commenter['comment_author_email']) . '" size="30"' . $aria_req . ' />' .
+        '</p><!-- #form-section-email .form-section -->',
+    'url' => '')),
+    'comment_field' => '<p class="comment-form-comment">' .
+        '<label for="comment">' . __('Сообщение:') . '</label>' .
+        '<textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>' .
+        '</p><!-- #form-section-comment .form-section -->',
+    'comment_notes_after' => '',
+);
+comment_form($comment_args); ?>
+<?php //comment_form(); ?>
+
 
